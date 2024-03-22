8d82f45133e51dafa9b1c81c1f1e740600cdb166
Nakato-Musana-Zaina <nakatozainamusana@gmail.com>
2024-03-22 11:50:34 +0300

    attempted other question

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@ fun main(args: Array<String>) {
     students("Nakato", "Adalab")
     area(20,30)
     bmi(160.0, 80.0)
+    println(information(20,"Nakato", "green"))
+    arr(arrayOf("hens","Goats","camels","chicks","antelope"))
 }
 // create a function that takes it both the name and class of a student in AkiraChix
 fun students(name:String,classroom:String){
@@ -35,4 +37,15 @@ fun bmi(height:Double,weight:Double){
     println(bMI)
 }
 
+//Write a program that propmts the user to enter their name , age , favorite color, and then prints out a message including all their information.
+fun information(age:Int, name: String, favoriteColor : String){
+    var sentences = "Hello my name is $name , i am $age years old and my color is $favoriteColor"
+    println(sentences)
+}
+//Write a function that takes an array of strings as a parameter and returns the length of the longest string.
+fun arr(words: Array<String>){
+    println(words.maxOrNull())
+}
+
+//Write a function that takes a list of strings as a parameter and returns a new list with all the strings in uppercase.
 
